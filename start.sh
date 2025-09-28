47b89087c71c47fa8ecbf5842829f648255860ad
Daniella <daniellaguerra@uninorte.edu.co>
2025-09-28 16:49:44 -0500

    acc port

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 uvicorn app.main:app --host 0.0.0.0 --port 8000 &
 
 # Lanzar Streamlit
-streamlit run app/ui.py --server.port 8501 --server.address 0.0.0.0
+streamlit run app/ui.py --server.port 10000 --server.address 0.0.0.0
